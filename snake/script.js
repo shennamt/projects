de911dbde5523c36090e612c945ae3d1ea7ae3e1
Shenna Mae Tan <shenmae.t@gmail.com>
2022-12-10 01:08:19 +0800

    feat: game over alert

--- snake/script.js
+++ snake/script.js
@@ -194,8 +194,25 @@ $(() => {
         }).length > 0 || x < 0 || x > canvas.width || y < 0 || y > canvas.height; // it returns an array, so if that array length is greater than 0, then it's the body etc.
     };
 
+    function displayGameOver() {
+        ctx.font = "43px 'Press Start 2P', cursive";
+        ctx.fillStyle = 'red';
+        ctx.fillText("GAME OVER", 50, 265);
+    }
+
+    function resetSnake() {
+        score = 0;
+        snake = [
+            {x: 50, y: 100, oldX: 0, oldY: 0},
+            {x: 51, y: 100, oldX: 0, oldY: 0},
+            {x: 52, y: 100, oldX: 0, oldY: 0}
+        ];
+        let food = {x: 200, y: 200, eaten: false};
+        gameLoop();
+    }
+
     function gameOver() {
         clearInterval(game);
-        alert('Game over');
+        displayGameOver();
     };
 });
