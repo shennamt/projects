66ac1056a9d55b9416bc3bb18cc174898dc75b3e
Shenna Mae Tan <shenmae.t@gmail.com>
2023-02-03 17:21:05 +0800

    feat: created components

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-     <h1>Mic test</h1>
+      <h1>Mic test</h1>
+
+      {/* homepage with the search bar */}
+      <Home />
+      
+      {/* search page with results */}
+
     </div>
   );
 }
---
+++ google-clone/src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>This is homepage</h1>
+
+      <div className="home_header">
+
+      </div>
+
+      <div className="home_body">
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default Home
