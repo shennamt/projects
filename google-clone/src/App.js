7659e5ba1b75b73991c1167661e120a014f2ddac
Shenna Mae Tan <shenmae.t@gmail.com>
2023-02-03 19:51:33 +0800

    feat: home component aka page

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Home from './Pages/Home';
 
 function App() {
     return (
         <div className="App">
             <h1>Mic Test</h1>
+
+            {/* Home page */}
+            <Home />
+
+            {/* Search page (result) */}
+
         </div>
     );
 }
