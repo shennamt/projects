d3422e69cac19973240e603355e4675bd15da162
Shenna Mae Tan <shenmae.t@gmail.com>
2023-02-04 00:39:11 +0800

    feat: making the buttons work with useState

--- google-clone/src/Components/Search.js
+++ google-clone/src/Components/Search.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Search.css'
 import { Link } from "react-router-dom";
 
 const Search = () => {
-  return (
-    <div className='search'>
-        <div className='search__input'>
-            <h6>Search</h6>
-            <input/>
-            <h6>Mic</h6>
-        </div>
-        <div className='search__buttons'>
-            <button>Google Search</button>
-            <button>I'm Feeling Lucky</button>
-        </div>
-    </div>
-  )
+    // whatever is keyed into the search box is captured in input
+    const [input, setInput] = useState('');
+
+    const search = (e) => {
+        e.preventDefault();
+        console.log('search button was clicked >>', input)
+    }
+    return (
+        <form className='search'>
+            <div className='search__input'>
+                <h6>Search</h6>
+                <input value={input} onChange={e => setInput(e.target.value)}/>
+                <h6>Mic</h6>
+            </div>
+
+            <div className='search__buttons'>
+                <button type='submit' onClick={search}>Google Search</button>
+                <button>I'm Feeling Lucky</button>
+            </div>
+        </form>
+    )
 }
 
 export default Search
