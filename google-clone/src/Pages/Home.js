a2a7773731d03382cd0b90d4a4b3a8d868033998
Shenna Mae Tan <shenmae.t@gmail.com>
2023-02-03 19:59:06 +0800

    feat: home component update and MUI install

--- google-clone/src/Pages/Home.js
+++ google-clone/src/Pages/Home.js
@@ -3,9 +3,25 @@ import './Home.css'
 
 function Home() {
     return (
-        <div className="home">
+        <div className='home'>
             <h1>Homepage</h1>
-      
+            <div className='home__header'>
+                <div className='home__headerLeft'>
+                    {/* About Link */}
+                    {/* Store Link */}
+                </div>
+                <div className='home__headerRight'>
+                    {/* Gmail Link */}
+                    {/* Images Link */}
+                    {/* Icon Link */}
+                    {/* Avatar Link */}
+                </div>
+
+            </div>
+
+            <div className='home__body'>
+
+            </div>
         </div>
     )
 }
