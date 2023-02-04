e2ad010cf3a4cc750e16de53233f5be5c2ee8a9a
Shenna Mae Tan <shenmae.t@gmail.com>
2023-02-04 14:02:34 +0800

    feat: more homepage functionality prep

--- proj2/google-clone/src/App.js
+++ proj2/google-clone/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './Pages/Home';
 
 function App() {
     return (
--- proj2/google-clone/src/Pages/Home.js
+++ proj2/google-clone/src/Pages/Home.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import '/Home.css';
+import './Home.css';
 
 function Home() {
     return (
         <div className='home'>
-        
+            <h1>Homepage</h1>
+
+            <div className='home__header'>
+                {/* about, store, all the way to avatar */}
+                <div className='home__headerLeft'>
+                    {/* About Link */}
+                    {/* Store Link */}
+                </div>
+
+                <div className='home__headerRight'>
+                    {/* Gmail Link */}
+                    {/* Images Link */}
+                    {/* Icon */}
+                    {/* Avatar */}
+                </div>
+            </div>
+
+            <div className='home__body'>
+                {/* google logo, search bars and buttons */}
+            </div>
         </div>
     )
 }
