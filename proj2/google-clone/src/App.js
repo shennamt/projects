4f706b01037904e5ac1127b9ed8266fddbb3cef8
Shenna Mae Tan <shenmae.t@gmail.com>
2023-02-04 13:41:02 +0800

    feat: home component

--- proj2/google-clone/src/App.js
+++ proj2/google-clone/src/App.js
@@ -4,6 +4,10 @@ function App() {
     return (
         <div className="App">
             <h1>Hello world</h1>
+            {/* home page, component 1*/}
+            <Home />
+            {/* search page, AKA search result component 2 */}
+
         </div>
     );
 };
