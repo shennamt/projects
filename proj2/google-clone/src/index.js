b4266ba540a32ddefe3a9bba4041a83616896739
Shenna Mae Tan <shenmae.t@gmail.com>
2023-02-10 00:39:00 +0800

    feat: setting up data layer

--- proj2/google-clone/src/index.js
+++ proj2/google-clone/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reducer, {initialState} from './reducer';
+import {StateProvider} from './StateProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        {/* wrapping the app in a data layer so that we can push and pull whenever */}
         <StateProvider initialState={initialState} reducer={reducer}>
         <App />
         </StateProvider>
