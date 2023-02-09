1105d98e1a28daa52a6c8a74aad4c03ed9a0ba09
Shenna Mae Tan <shenmae.t@gmail.com>
2023-02-09 10:38:07 +0800

    feat: home page logo

--- proj2/google-clone/src/Pages/Home.js
+++ proj2/google-clone/src/Pages/Home.js
@@ -7,25 +7,22 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 function Home() {
     return (
         <div className='home'>
-            <h1>Homepage</h1>
-
             <div className='home__header'>
                 {/* using links intead of a tags to avoid refresh */}
                 <div className='home__headerLeft'>
                     <Link to='/about'>About</Link>
                     <Link to='/store'>Store</Link>
                 </div>
-
                 <div className='home__headerRight'>
                     <Link to='/gmail'>Gmail</Link>
                     <Link to='/images'>Images</Link>
-                    <AppsIcon />
+                    <AppsIcon style={{ marginRight: '20px'}} />
                     <AccountCircleIcon />
                 </div>
             </div>
-
             <div className='home__body'>
                 {/* google logo, search bars and buttons */}
+                <img src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg" alt=""/>
             </div>
         </div>
     )
