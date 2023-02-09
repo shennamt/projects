b1f947493556eb6fb0e6c0be7bcf532227739d55
Shenna Mae Tan <shenmae.t@gmail.com>
2023-02-09 19:21:12 +0800

    feat: buttons added

--- proj2/google-clone/src/Components/Search.js
+++ proj2/google-clone/src/Components/Search.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './Search.css'
-import MicNoneIcon from '@mui/icons-material/MicNone';
+import MicIcon from '@mui/icons-material/Mic';
 import SearchIcon from '@mui/icons-material/Search';
+import Button from '@mui/material/Button';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
 
 const Search = () => {
     return (
@@ -9,7 +11,13 @@ const Search = () => {
             <div className='search__input'>
                 <SearchIcon className='search__inputIcon' />
                 <input />
-                <MicNoneIcon />
+                <MicIcon />
+                <CameraAltIcon />
+            </div>
+
+            <div className='search__buttons'>
+                <Button variant="outlined">Google Search</Button>
+                <Button variant="outlined">I'm Feeling Lucky</Button>
             </div>
         </div>
     )
